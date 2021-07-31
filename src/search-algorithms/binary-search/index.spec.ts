d0b934044a403a464b7e8cfcf16a0466b20d2e01
edmilson-dk <droidvidaboa@gmail.com>
2021-07-30 23:42:26 -0300

    test: should return -1 if item is not found

--- src/search-algorithms/binary-search/index.spec.ts
+++ src/search-algorithms/binary-search/index.spec.ts
@@ -13,6 +13,18 @@ describe("Binary search string tests", () => {
     });
     expect(index).toBe(30);
   });
+
+  it("should return -1 if item is not found", () => {
+    const array = generateArrayOfString(40, "flag");
+    const wanted = "flag50";
+    const index = binarySearchStr({
+      array,
+      left: 0,
+      right: array.length - 1,
+      wanted
+    });
+    expect(index).toBe(-1);
+  });
 });
 
 describe("Binary search number tests", () => {
