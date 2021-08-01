02857b39aa57b907e83f70ca00ccd19be3d30033
edmilson-dk <droidvidaboa@gmail.com>
2021-08-01 13:47:26 -0300

    test: should return the index of the element if it is present

---
+++ src/search-algorithms/jump-search/index.spec.ts
@@ -0,0 +1,18 @@
+import { generateSortedArrayOfNumber } from "../../__tests__/utils";
+import { jumpSearch } from "./index";
+
+describe("Jump searc tests", () => {
+  it("should return the index of the element if it is present", () => {
+    const array = generateSortedArrayOfNumber(40);
+    const wanted = 34;
+    const arrayLength = array.length;
+
+    const index = jumpSearch({
+      array,
+      wanted,
+      arrayLength
+    });
+
+    expect(index).toBe(34);
+  });
+});
