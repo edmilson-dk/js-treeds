13ee16ed425f948921bd839c57084729c05adc7b
edmilson-dk <droidvidaboa@gmail.com>
2021-08-01 18:45:14 -0300

    exported modules

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from "./search-algorithms/linear-search";
 export * from "./search-algorithms/binary-search";
+export * from "./search-algorithms/interpolation-search";
+export * from "./search-algorithms/jump-search";
