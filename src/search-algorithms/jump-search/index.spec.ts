b1219a0c15cd65d10a56f1c95b58e91eb3aa2ee0
edmilson-dk <droidvidaboa@gmail.com>
2021-08-01 13:50:47 -0300

    test: should return -1 if the element is not present

--- src/search-algorithms/jump-search/index.spec.ts
+++ src/search-algorithms/jump-search/index.spec.ts
@@ -15,4 +15,18 @@ describe("Jump searc tests", () => {
 
     expect(index).toBe(34);
   });
+
+  it("should return -1 if the element is not present", () => {
+    const array = generateSortedArrayOfNumber(40);
+    const wanted = 45;
+    const arrayLength = array.length;
+
+    const index = jumpSearch({
+      array,
+      wanted,
+      arrayLength
+    });
+
+    expect(index).toBe(-1);
+  });
 });
