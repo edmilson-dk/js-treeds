032edadbbf893fa98c9b98bc0b5b2d2c2d2b3b9e
edmilson-dk <droidvidaboa@gmail.com>
2021-08-04 10:24:48 -0300

    test: should return the index of the element if it is present

--- src/search-algorithms/exponential-search/index.spec.ts
+++ src/search-algorithms/exponential-search/index.spec.ts
@@ -2,7 +2,7 @@ import { generateSortedArrayOfNumber } from "../../__tests__/utils";
 import { exponentialSearch } from "./index";
 
 describe("Exponential Search tests", () => {
-    it("should return the index of the element if it is present", () => {
+  it("should return the index of the element if it is present", () => {
     const array = generateSortedArrayOfNumber(40);
     const wanted = 3;
   
