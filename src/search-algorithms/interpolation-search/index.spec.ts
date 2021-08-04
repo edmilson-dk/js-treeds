99c66d759df4218ee4bd0a8b81dbbfd151818ee5
edmilson-dk <droidvidaboa@gmail.com>
2021-08-04 09:49:22 -0300

    test: should return the inde xof element if it is present in last position of the array

--- src/search-algorithms/interpolation-search/index.spec.ts
+++ src/search-algorithms/interpolation-search/index.spec.ts
@@ -16,6 +16,20 @@ describe("Interpolation Search tests", () => {
     expect(index).toBe(39);
   });
 
+  it("should return the inde xof element if it is present in last position of the array", () => {
+    const array = generateSortedArrayOfNumber(40);
+    const wanted = 39;
+    const arrayLength = array.length;
+
+    const index = interpolationSearch({
+      array,
+      wanted,
+      endIndex: arrayLength - 1,
+      startIndex: 0
+    });
+    expect(index).toBe(39);
+  })
+
   it("should return -1 if the element is not present", () => {
     const array = generateSortedArrayOfNumber(40);
     const wanted = 41;
