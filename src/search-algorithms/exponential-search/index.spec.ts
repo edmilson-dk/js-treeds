cc63fbb9b916b8d68c7aa3bdee787796c32b0944
edmilson-dk <droidvidaboa@gmail.com>
2021-08-04 10:25:35 -0300

    test: should return -1 if the element is not present

--- src/search-algorithms/exponential-search/index.spec.ts
+++ src/search-algorithms/exponential-search/index.spec.ts
@@ -5,11 +5,22 @@ describe("Exponential Search tests", () => {
   it("should return the index of the element if it is present", () => {
     const array = generateSortedArrayOfNumber(40);
     const wanted = 3;
-  
+
     const index = exponentialSearch({
       array,
-      wanted
+      wanted,
     });
     expect(index).toBe(3);
   });
-})
+
+  it("should return -1 if the element is not present", () => {
+    const array = generateSortedArrayOfNumber(40);
+    const wanted = 50;
+
+    const index = exponentialSearch({
+      array,
+      wanted,
+    });
+    expect(index).toBe(-1);
+  });
+});
