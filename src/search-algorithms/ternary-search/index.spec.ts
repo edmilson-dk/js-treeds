8de56d52e7b3ead237af3b745512dbf765a02687
edmilson-dk <droidvidaboa@gmail.com>
2021-08-04 11:51:00 -0300

    test: should return -1 if item is not found

--- src/search-algorithms/ternary-search/index.spec.ts
+++ src/search-algorithms/ternary-search/index.spec.ts
@@ -14,4 +14,16 @@ describe("Ternary Search tests", () => {
     });
     expect(index).toBe(36);
   });
+
+  it("should return -1 if item is not found", () => {
+    const array = generateSortedArrayOfNumber(40);
+    const wanted = 42;
+    const index = ternarySearch({
+      array,
+      left: 0,
+      right: array.length,
+      wanted,
+    });
+    expect(index).toBe(-1);
+  });
 });
