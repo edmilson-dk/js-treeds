e65fc97582213465e5775937acb5a4b347666462
edmilson-dk <droidvidaboa@gmail.com>
2021-08-04 10:06:37 -0300

    test: should return the of element if it is present in first position of the array

--- src/search-algorithms/interpolation-search/index.spec.ts
+++ src/search-algorithms/interpolation-search/index.spec.ts
@@ -4,7 +4,7 @@ import { interpolationSearch } from "./index";
 describe("Interpolation Search tests", () => {
   it("should return the index of the element if it is present", () => {
     const array = generateSortedArrayOfNumber(40);
-    const wanted = 39;
+    const wanted = 20;
     const arrayLength = array.length;
 
     const index = interpolationSearch({
@@ -13,10 +13,24 @@ describe("Interpolation Search tests", () => {
       endIndex: arrayLength - 1,
       startIndex: 0
     });
-    expect(index).toBe(39);
+    expect(index).toBe(20);
   });
 
-  it("should return the inde xof element if it is present in last position of the array", () => {
+  it("should return the of element if it is present in first position of the array", () => {
+    const array = generateSortedArrayOfNumber(40);
+    const wanted = 0;
+    const arrayLength = array.length;
+
+    const index = interpolationSearch({
+      array,
+      wanted,
+      endIndex: arrayLength - 1,
+      startIndex: 0
+    });
+    expect(index).toBe(0);
+  })
+
+  it("should return the index of element if it is present in last position of the array", () => {
     const array = generateSortedArrayOfNumber(40);
     const wanted = 39;
     const arrayLength = array.length;
