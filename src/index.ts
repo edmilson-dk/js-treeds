2a8b4d3e392edf8c7d708407ca4a71173aa97c6d
edmilson-dk <droidvidaboa@gmail.com>
2021-08-06 11:16:51 -0300

    feat: added selection sort export algorithms

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+// search algorithms
 export * from "./search-algorithms/linear-search";
 export * from "./search-algorithms/binary-search";
 export * from "./search-algorithms/interpolation-search";
 export * from "./search-algorithms/jump-search";
 export * from "./search-algorithms/exponential-search";
 export * from "./search-algorithms/ternary-search";
+
+// sort algorithms
+export * from "./sort-algorithms/selection-sort";
