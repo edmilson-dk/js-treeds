67943b6fbca0b587a4a7f9231b741b43310c9f43
edmilson-dk <droidvidaboa@gmail.com>
2021-08-10 20:56:22 -0300

    test: should return sorted array when array contains floating numbers

--- src/sort-algorithms/insertion-sort/index.spec.ts
+++ src/sort-algorithms/insertion-sort/index.spec.ts
@@ -2,7 +2,9 @@ import {
   arrayNotOrdenatedWithRepeatedElements,
   arrayOrdenatedWithRepeatedElements,
   generateNotSortedArrayOfNumber,
+  generateNotSortedArrayOfNumbersFloating,
   generateSortedArrayOfNumber,
+  generateSortedArrayOfNumbersFloating,
 } from "../../__tests__/utils";
 import { insertionSort } from "./index";
 
@@ -53,6 +55,15 @@ describe("Insertion sort tests", () => {
   it("should return sorted big array", () => {
     const array = generateNotSortedArrayOfNumber(20000);
     const sortedArray = generateSortedArrayOfNumber(20000);
+
+    insertionSort({ array });
+
+    expect(array).toEqual(sortedArray);
+  });
+
+  it("should return sorted array when array contains floating numbers", () => {
+    const array = generateNotSortedArrayOfNumbersFloating(30);
+    const sortedArray = generateSortedArrayOfNumbersFloating(30);
 
     insertionSort({ array });
 
