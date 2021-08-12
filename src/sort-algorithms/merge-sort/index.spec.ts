e06cef58ad9bd9405cdc7d9087cb532f9aed3c82
edmilson-dk <droidvidaboa@gmail.com>
2021-08-11 22:04:51 -0300

    test: should return sorted array when array contains negative numbers

--- src/sort-algorithms/merge-sort/index.spec.ts
+++ src/sort-algorithms/merge-sort/index.spec.ts
@@ -38,4 +38,17 @@ describe("Merge sort tests", () => {
 
     expect(array).toEqual([]);
   });
+
+  it("should return sorted array when array contains negative numbers", () => {
+    const array = generateSortedArrayOfNumber(30, true);
+    const sortedArray = generateNotSortedArrayOfNumber(30, true);
+
+    mergeSort({
+      array,
+      leftIndex: 0,
+      rightIndex: array.length - 1,
+    });
+
+    expect(array).toEqual(sortedArray);
+  });
 });
