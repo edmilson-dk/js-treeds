3d5772cade7f7bb7145f600df354e504f48a7153
edmilson-dk <droidvidaboa@gmail.com>
2021-08-11 22:17:25 -0300

    test: should return an ordered array when the array contains repeated elements

--- src/sort-algorithms/merge-sort/index.spec.ts
+++ src/sort-algorithms/merge-sort/index.spec.ts
@@ -1,4 +1,9 @@
-import { generateNotSortedArrayOfNumber, generateSortedArrayOfNumber } from "../../__tests__/utils";
+import {
+  arrayNotOrdenatedWithRepeatedElements,
+  arrayOrdenatedWithRepeatedElements,
+  generateNotSortedArrayOfNumber,
+  generateSortedArrayOfNumber,
+} from "../../__tests__/utils";
 import { mergeSort } from "./index";
 
 describe("Merge sort tests", () => {
@@ -42,6 +47,19 @@ describe("Merge sort tests", () => {
   it("should return sorted array when array contains negative numbers", () => {
     const array = generateSortedArrayOfNumber(30, true);
     const sortedArray = generateNotSortedArrayOfNumber(30, true);
+
+    mergeSort({
+      array,
+      leftIndex: 0,
+      rightIndex: array.length - 1,
+    });
+
+    expect(array).toEqual(sortedArray);
+  });
+
+  it("should return an ordered array when the array contains repeated elements", () => {
+    const array = arrayNotOrdenatedWithRepeatedElements;
+    const sortedArray = arrayOrdenatedWithRepeatedElements;
 
     mergeSort({
       array,
