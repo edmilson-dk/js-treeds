5998b77ccd25534571e119afa2f88d8c131f737b
edmilson-dk <droidvidaboa@gmail.com>
2021-08-16 22:17:01 -0300

    test: should return sorted array when array constains generics values

--- src/sort-algorithms/quick-sort/index.spec.ts
+++ src/sort-algorithms/quick-sort/index.spec.ts
@@ -5,6 +5,8 @@ import {
   generateNotSortedArrayOfNumbersFloating,
   generateSortedArrayOfNumber,
   generateSortedArrayOfNumbersFloating,
+  genericArrayNotOrdenated,
+  genericArrayOrdenated,
 } from "../../__tests__/utils";
 import { quickSort } from "./index";
 
@@ -75,6 +77,19 @@ describe("Quick sort tests", () => {
   it("should return sorted array when array contains floating numbers", () => {
     const array = generateNotSortedArrayOfNumbersFloating(30);
     const sortedArray = generateSortedArrayOfNumbersFloating(30);
+
+    quickSort({
+      array,
+      low: 0,
+      high: array.length - 1,
+    });
+
+    expect(array).toEqual(sortedArray);
+  });
+
+  it("should return sorted array when array constains generics values", () => {
+    const array = genericArrayNotOrdenated;
+    const sortedArray = genericArrayOrdenated;
 
     quickSort({
       array,
