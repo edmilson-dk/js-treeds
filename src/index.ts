c1f1d68229ecfc5a4c83abfeb256e6b33e77c62d
edmilson-dk <droidvidaboa@gmail.com>
2021-08-18 22:42:56 -0300

    feat: added export quick sort file

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,6 @@ export * from "./sort-algorithms/bubble-sort";
 export * from "./sort-algorithms/insertion-sort";
 export * from "./sort-algorithms/merge-sort";
 export * from "./sort-algorithms/heap-sort";
-// export * from "./sort-algorithms/quick-sort";
+export * from "./sort-algorithms/quick-sort";
 // export * from "./sort-algorithms/radix-sort";
 // export * from "./sort-algorithms/shell-sort";
