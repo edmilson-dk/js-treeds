1699722208cc7a8a9ce14736abc3335f7709f3df
edmilson-dk <droidvidaboa@gmail.com>
2021-08-25 09:42:27 -0300

    test: should return an ordered array when the array contains repeated elements

--- src/sort-algorithms/couting-sort/index.spec.ts
+++ src/sort-algorithms/couting-sort/index.spec.ts
@@ -1,10 +1,24 @@
-import { generateNotSortedArrayOfNumber, generateSortedArrayOfNumber } from "../../__tests__/utils";
+import {
+  arrayNotOrdenatedWithRepeatedElements,
+  arrayOrdenatedWithRepeatedElements,
+  generateNotSortedArrayOfNumber,
+  generateSortedArrayOfNumber,
+} from "../../__tests__/utils";
 import { countSort } from "./index";
 
 describe("Counting sort tests", () => {
   it("must return an array ordered from smallest to largest", () => {
     const array = generateNotSortedArrayOfNumber(100);
     const sortedArray = generateSortedArrayOfNumber(100);
+
+    countSort(array);
+
+    expect(array).toEqual(sortedArray);
+  });
+
+  it("should return an ordered array when the array contains repeated elements", () => {
+    const array = arrayNotOrdenatedWithRepeatedElements;
+    const sortedArray = arrayOrdenatedWithRepeatedElements;
 
     countSort(array);
 
