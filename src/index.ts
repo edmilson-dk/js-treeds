8825776500759bfeb0ac7a43d9fb5595aa4acee7
edmilson-dk <droidvidaboa@gmail.com>
2021-08-29 20:56:08 -0300

    feat: added news comments

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,32 @@ export * from "./sort-algorithms/counting-sort";
 // export * from "./sort-algorithms/gog-sort";
 // export * from "./sort-algorithms/gnome-sort";
 // export * from "./sort-algorithms/heap-sort";
+
+// data structures
+
+// export * from "./data-structures/linked-list";
+// export * from "./data-structures/stack";
+// export * from "./data-structures/queue";
+// export * from "./data-structures/priority-queue";
+// export * from "./data-structures/binary-search-tree";
+// export * from "./data-structures/binary-heap";
+// export * from "./data-structures/hash-table";
+// export * from "./data-structures/hash-map";
+// export * from "./data-structures/hash-set";
+// export * from "./data-structures/hash-table-chain";
+// export * from "./data-structures/hash-table-separate-chaining";
+// export * from "./data-structures/hash-table-open-addressing";
+// export * from "./data-structures/hash-table-linear-probing";
+// export * from "./data-structures/hash-table-quadratic-probing";
+// export * from "./data-structures/hash-table-double-hashing";
+// export * from "./data-structures/hash-table-double-hashing-with-chaining";
+// export * from "./data-structures/hash-table-double-hashing-with-open-addressing";
+// export * from "./data-structures/hash-table-double-hashing-with-separate-chaining";
+// export * from "./data-structures/hash-table-double-hashing-with-linear-probing";
+// export * from "./data-structures/hash-table-double-hashing-with-quadratic-probing";
+// export * from "./data-structures/hash-table-double-hashing-with-double-hashing";
+// export * from "./data-structures/hash-table-double-hashing-with-double-hashing-with-chaining";
+// export * from "./data-structures/hash-table-double-hashing-with-double-hashing-with-open-addressing";
+// export * from "./data-structures/avl-tree";
+// export * from "./data-structures/heap";
+// export * from "./data-structures/splay-tree";
